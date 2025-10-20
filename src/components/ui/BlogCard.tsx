9b001b4dc96eea87ea25124ef11772aac5e8263a
bestnp <narawit.p40@gmail.com>
2025-10-20 23:29:06 +0700

    feat: add search functionality and blog post filtering to ArticleSection component
    - Implemented search input to filter blog posts by title and description.
    - Added logic to filter posts based on selected category and search term.
    - Displayed filtered blog posts in a grid layout with a message for no results.

---
+++ src/components/ui/BlogCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { BlogPost } from "@/data/blogPost";
+
+interface BlogCardProps {
+  post: BlogPost;
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({ post }) => {
+  const getCategoryColor = (category: string) => {
+    switch (category.toLowerCase()) {
+      case 'cat':
+        return 'bg-green-light text-green rounded-full px-3 py-1 text-sm font-semibold';
+      case 'general':
+        return 'bg-brown-200 text-brown-600 rounded-full px-3 py-1 text-sm font-semibold';
+      case 'inspiration':
+        return 'bg-orange text-white rounded-full px-3 py-1 text-sm font-semibold';
+      default:
+        return 'bg-brown-200 text-brown-600 rounded-full px-3 py-1 text-sm font-semibold';
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <a href="#" className="relative h-[212px] sm:h-[360px]">
+        <img 
+          className="w-full h-full object-cover rounded-md" 
+          src={post.image} 
+          alt={post.title}
+        />
+      </a>
+      <div className="flex flex-col">
+        <div className="flex">
+          <span className={`${getCategoryColor(post.category)} mb-2`}>
+            {post.category}
+          </span>
+        </div>
+        <a href="#">
+          <h2 className="text-start font-bold text-xl mb-2 line-clamp-2 hover:underline text-brown-600">
+            {post.title}
+          </h2>
+        </a>
+        <p className="text-brown-400 text-sm mb-4 flex-grow line-clamp-3">
+          {post.description}
+        </p>
+        <div className="flex items-center text-sm text-brown-500">
+          <span>{post.author}</span>
+          <span className="mx-2 text-brown-300">|</span>
+          <span>{post.date}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
